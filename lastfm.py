530e79c031a89444f3d995ccebda91c73f533fb4
LostB053 <72547660+lostb053@users.noreply.github.com>
2021-02-21 20:33:44 +0530

    Update lastfm.py

--- lastfm.py
+++ lastfm.py
@@ -3,14 +3,13 @@
 # Copyright (C) 2020 BY USERGE-X.
 # All rights reserved.
 # Inspiration from @lastfmrobot <mainly> (owned by @dangou on telegram) and...
-# @TheRealPhoenixBot(owned by @TheRealPhoenix on telegram and github user as rsktg)
-# Code re-written by @DeletedUser420 (telegram), github user as code-rgb
+# TheRealPhoenixBot owned by rsktg
+# Code re-written by code-rgb
 
 import asyncio
 
 from userge import Config, Message, get_collection, userge
-from userge.lastfm import auth_, get_response, pcurl, ripimg, tglst, user
-from userge.utils import rand_array
+from userge.lastfm import auth_, get_response, pcurl, rand, ripimg, tglst, user
 
 du = "https://last.fm/user/"
 
@@ -40,14 +39,14 @@ async def toggle_lastfm_profile_(message: Message):
 @userge.on_cmd(
     "lp",
     about={
-        "header": "Get Lastfm now playing pic",
+        "header": "Get Lastfm now playing",
         "usage": "{tr}lp [lastfm username] (optional)",
     },
 )
-async def last_fm_pic_(message: Message):
+async def last_fm_(message: Message):
     """Currently Playing"""
     query = message.input_str or Config.LASTFM_USERNAME
-    params = {
+    params={
         "method": "user.getrecenttracks",
         "user": query,
         "limit": 3,
@@ -63,14 +62,14 @@ async def last_fm_pic_(message: Message):
     if recent_song[0].get("@attr"):
         img = recent_song[0].get("image")[3].get("#text")
         if img in ripimg():
-            img = rand_array(pcurl())
+            img = rand(pcurl())
         rep = f"[\u200c]({img})**{qd}** is currently listening to:\n"
         song_ = recent_song[0]
         song_name = song_["name"]
         artist_name = song_["artist"]["name"]
         rep += f"🎧  <code>{artist_name} - {song_name}</code>"
         rep += ", ♥️" if song_["loved"] != "0" else ""
-        tgparam = {
+        tgparam={
             "method": "track.getInfo",
             "track": song_name,
             "artist": artist_name,
@@ -104,7 +103,7 @@ async def last_fm_pic_(message: Message):
 async def last_fm_user_info_(message: Message):
     """Shows User Info"""
     query = message.input_str or Config.LASTFM_USERNAME
-    params = {
+    params={
         "method": "user.getInfo",
         "user": query,
         "api_key": Config.LASTFM_API_KEY,
@@ -117,10 +116,10 @@ async def last_fm_user_info_(message: Message):
     qd = f"[{query}]({du}{query})" if message.input_str else await user()
     result += f"LastFM User Info for **{qd}**:\n**User:** {query}\n"
     name = lastuser.get("realname")
-    result += f" 🔰 **Name:** {name}\n" if name != "" else ""
-    result += f" 🎵 **Total Scrobbles:** {lastuser['playcount']}\n"
+    result += f"  🔰 **Name:** {name}\n" if name != "" else ""
+    result += f"  🎵 **Total Scrobbles:** {lastuser['playcount']}\n"
     country = lastuser.get("country")
-    result += f" 🌍 **Country:** {country}\n" if country != "None" else ""
+    result += f"  🌍 **Country:** {country}\n" if country != "None" else ""
     await message.edit(result)
 
 
@@ -134,7 +133,7 @@ async def last_fm_user_info_(message: Message):
 async def last_pc_(message: Message):
     """Shows Playcount"""
     query = message.input_str or Config.LASTFM_USERNAME
-    params = {
+    params={
         "method": "user.getInfo",
         "user": query,
         "api_key": Config.LASTFM_API_KEY,
@@ -158,7 +157,7 @@ async def last_pc_(message: Message):
 async def last_fm_loved_tracks_(message: Message):
     """Shows Liked Songs"""
     query = message.input_str or Config.LASTFM_USERNAME
-    params = {
+    params={
         "method": "user.getlovedtracks",
         "limit": 20,
         "user": query,
@@ -169,7 +168,7 @@ async def last_fm_loved_tracks_(message: Message):
     if len(tracks) == 0:
         return await message.edit("You Don't have any Loved tracks yet.")
     qd = f"[{query}]({du}{query})" if message.input_str else await user()
-    rep = f"**Favourite (♥️) Tracks for {qd}**"
+    rep = f"**{qd}'s Liked (♥️) Tracks**"
     for song_ in tracks:
         song_name = song_["name"]
         artist_name = song_["artist"]["name"]
@@ -187,8 +186,7 @@ async def last_fm_loved_tracks_(message: Message):
 async def last_fm_played_(message: Message):
     """Shows Recently Played Songs"""
     query = message.input_str or Config.LASTFM_USERNAME
-    qd = f"[{query}]({du}{query})" if message.input_str else await user()
-    params = {
+    params={
         "method": "user.getrecenttracks",
         "limit": 20,
         "extended": 1,
@@ -199,7 +197,8 @@ async def last_fm_played_(message: Message):
     recent_song = (await get_response(params))[1]["recenttracks"]["track"]
     if len(recent_song) == 0:
         return await message.err("No Recent Tracks found", del_in=5)
-    rep = f"**{qd}** recently played 🎵 songs:\n"
+    qd = f"[{query}]({du}{query})" if message.input_str else await user()
+    rep = f"**{qd}**'s recently played 🎵 songs:\n"
     for song_ in recent_song:
         song_name = song_["name"]
         artist_name = song_["artist"]["name"]
@@ -218,7 +217,7 @@ async def last_fm_played_(message: Message):
 async def last_fm_love_(message: Message):
     """Loves Currently Playing Song"""
     await message.edit("Loving Currently Playing...")
-    params = {
+    params={
         "method": "user.getrecenttracks",
         "limit": 2,
         "user": Config.LASTFM_USERNAME,
@@ -230,7 +229,7 @@ async def last_fm_love_(message: Message):
         return await message.err("No Currently Playing Track found", del_in=10)
     img = recent_song[0].get("image")[3].get("#text")
     if img in ripimg():
-        img = rand_array(pcurl())
+        img = rand(pcurl())
     song_ = recent_song[0]
     anm = song_["artist"]["#text"]
     snm = song_["name"]
@@ -250,7 +249,7 @@ async def last_fm_love_(message: Message):
 async def last_fm_unlove_(message: Message):
     """UnLoves Currently Playing Song"""
     await message.edit("UnLoving Currently Playing...")
-    params = {
+    params={
         "method": "user.getrecenttracks",
         "limit": 2,
         "user": Config.LASTFM_USERNAME,
@@ -262,7 +261,7 @@ async def last_fm_unlove_(message: Message):
         return await message.err("No Currently Playing Track found", del_in=10)
     img = recent_song[0].get("image")[3].get("#text")
     if img in ripimg():
-        img = rand_array(pcurl())
+        img = rand(pcurl())
     song_ = recent_song[0]
     anm = song_["artist"]["#text"]
     snm = song_["name"]
@@ -290,13 +289,11 @@ async def lastfm_compat_(message: Message):
 
     if not message.input_str:
         return await message.edit("Please check `{tr}help Compat`")
-    diff = "|" in message.input_str
-    us1, us2 = (
-        message.input_str.split("|") if diff else Config.LASTFM_USERNAME,
-        message.input_str,
-    )
+    msg = message.input_str
+    diff = "|" in msg
+    us1, us2 = msg.split("|") if diff else Config.LASTFM_USERNAME, msg
     display = f"**{us1 if diff else await user()}** and **[{us2}]({du}{us2})**"
-    params = {
+    params={
         "method": "user.getTopArtists",
         "limit": 500,
         "api_key": Config.LASTFM_API_KEY,
