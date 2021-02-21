683687f30f037153a83ec4906df014bc7410cc3f
LostB053 <72547660+lostb053@users.noreply.github.com>
2021-02-21 20:50:19 +0530

    Update lastfm.py

--- lastfm.py
+++ lastfm.py
@@ -73,7 +73,10 @@ async def last_fm_(message: Message):
         }
         gt = (await get_response(tgparam))[1]["track"]["toptags"]["tag"]
         y = [i.replace(" ", "_").replace("-", "_") for i in [tg["name"] for tg in gt]]
-        z = [k for k in y if k.lower() in tglst()]
+        z = []
+        for k in y:
+            if k.lower() in tglst():
+                z.append(k)
         neutags = " #".join(z[i] for i in range(min(len(z), 4)))
         if img in ripimg():
             img = rand(pcurl())
